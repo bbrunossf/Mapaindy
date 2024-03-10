8705d47bb0fa2e47840d5f2570daf0c9b9ecedee
Bruno Oliveira <85851492+bbrunossf@users.noreply.github.com>
2024-03-10 18:00:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ m.get_root().header.add_child(folium.Element('<script type="text/javascript">{}<
 
 # Criando um evento de clique no mapa que chama a função JavaScript
 m.add_child(folium.LatLngPopup())
-
+st.text_input(value=update_coord)
 # Criando um botão para adicionar o marcador
 if st.button('Adicionar marcador'):
   # Tentando converter a entrada em uma lista de números
@@ -45,6 +45,6 @@ if st.button('Adicionar marcador'):
   except:
     # Exibindo uma mensagem de erro
     st.error('Coordenada inválida. Clique em um ponto do mapa.')
-st.text_input(value=coord)
+   
 # Exibindo o mapa com o streamlit-folium
 folium_static(m)
