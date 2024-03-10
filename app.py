116b1e04928003398916ec17dfe0da493159df7e
Bruno Oliveira <85851492+bbrunossf@users.noreply.github.com>
2024-03-10 17:24:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,38 +6,28 @@ from streamlit_folium import folium_static
 # Criando o objeto Map com a localização e o zoom de Brasília
 m = folium.Map(location=[-15.793889, -47.882778], zoom_start=11)
 
-# Criando um elemento HTML para mostrar as coordenadas do mouse
-coordDIV = """
-<div id='coordDIV' style='position: fixed; bottom: 50px; left: 10px; z-index: 9999; background-color: white; padding: 5px; border: 1px solid black;'>
-Clique no mapa para ver as coordenadas
-</div>
-"""
+# Criando uma caixa de texto para receber a coordenada do usuário
+coord = st.text_input('Digite a coordenada do marcador (ex: [-15.7589665, -47.879422])', '[-15.7589665, -47.879422]', type='default')
 
-# Adicionando o elemento HTML ao mapa
-m.get_root().html.add_child(folium.Element(coordDIV))
-
-# Criando uma função JavaScript para atualizar o elemento HTML com as coordenadas do clique
-update_coordDIV = """
-function updateCoordDIV(e) {
-  var coordDIV = document.getElementById('coordDIV');
-  var lat = e.latlng.lat.toFixed(6);
-  var lng = e.latlng.lng.toFixed(6);
-  coordDIV.innerHTML = 'Latitude: ' + lat + '<br>Longitude: ' + lng;
-}
-"""
-
-# Adicionando a função JavaScript ao mapa
-m.get_root().header.add_child(folium.Element('<script type="text/javascript">{}</script>'.format(update_coordDIV)))
-
-# Criando um evento de clique no mapa que chama a função JavaScript
-m.add_child(folium.LatLngPopup())
-
-# Criando uma função Python para criar um marcador com o Folium a partir das coordenadas do clique
-def add_marker(e):
-  folium.Marker(location=[e.latlng.lat, e.latlng.lng], popup='Marcador').add_to(m)
-
-# Adicionando um evento de clique no mapa que chama a função Python
-m.add_child(folium.ClickForMarker(popup='Marcador'))
+# Criando um botão para adicionar o marcador
+if st.button('Adicionar marcador'):
+  # Tentando converter a entrada em uma lista de números
+  try:
+    coord = eval(coord)
+    # Verificando se a entrada é uma lista de dois números
+    if isinstance(coord, list) and len(coord) == 2 and all(isinstance(x, (int, float)) for x in coord):
+      # Criando o objeto Marker com a coordenada, o popup e o ícone
+      marker = folium.Marker(location=coord, popup='Marcador', icon=folium.Icon(color='red', icon='info-sign'))
+      # Adicionando o marcador ao mapa
+      marker.add_to(m)
+      # Exibindo uma mensagem de sucesso
+      st.success('Marcador adicionado com sucesso!')
+    else:
+      # Exibindo uma mensagem de erro
+      st.error('Coordenada inválida. Digite uma lista de dois números separados por vírgula.')
+  except:
+    # Exibindo uma mensagem de erro
+    st.error('Coordenada inválida. Digite uma lista de dois números separados por vírgula.')
 
 # Exibindo o mapa com o streamlit-folium
 folium_static(m)
