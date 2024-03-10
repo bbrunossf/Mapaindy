12cc294c76775b660b8f3ee76aa7eebeefaa8025
Bruno Oliveira <85851492+bbrunossf@users.noreply.github.com>
2024-03-10 17:12:48 -0300

    Update app.py 3

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
+# Importando o streamlit, o Folium e o streamlit-folium
 import streamlit as st
 import folium
 from streamlit_folium import folium_static
 
-def main():
-    st.title("Adicionar Marcador no Mapa")
+# Criando o objeto Map com a localização e o zoom de Brasília
+m = folium.Map(location=[-15.793889, -47.882778], zoom_start=11)
 
-    # Criando um mapa com o Folium
-    m = folium.Map(location=[-15.793889, -47.882778], zoom_start=10)
+# Criando um elemento HTML para mostrar as coordenadas do mouse
+coordDIV = """
+<div id='coordDIV' style='position: fixed; bottom: 50px; left: 10px; z-index: 9999; background-color: white; padding: 5px; border: 1px solid black;'>
+Clique no mapa para ver as coordenadas
+</div>
+"""
 
-    # Função para adicionar marcador no mapa
-    def add_marker(lat, lon):
-        folium.Marker([lat, lon], popup=f"Coordenadas: {lat}, {lon}").add_to(m)
-        folium.CircleMarker([lat, lon], radius=5, color='blue', fill_color='blue').add_to(m)
-        st.write("Marcador adicionado com sucesso!")
+# Adicionando o elemento HTML ao mapa
+m.get_root().html.add_child(folium.Element(coordDIV))
 
-    # Streamlit
-    col1, col2 = st.columns([3, 1])
+# Criando uma função JavaScript para atualizar o elemento HTML com as coordenadas do clique
+update_coordDIV = """
+function updateCoordDIV(e) {
+  var coordDIV = document.getElementById('coordDIV');
+  var lat = e.latlng.lat.toFixed(6);
+  var lng = e.latlng.lng.toFixed(6);
+  coordDIV.innerHTML = 'Latitude: ' + lat + '<br>Longitude: ' + lng;
+}
+"""
 
-    # Renderizando o mapa
-    with col1:
-        folium_static(m)
+# Adicionando a função JavaScript ao mapa
+m.get_root().header.add_child(folium.Element('<script type="text/javascript">{}</script>'.format(update_coordDIV)))
 
-    # Botão para adicionar marcador
-    with col2:
-        st.write("Clique no mapa para adicionar um marcador")
-        # Utilizando st.map para mostrar o mapa e capturar as coordenadas clicadas
-        map_clicked = st.map(m)
-        lat, lon = map_clicked.pydeck_chart.get_selection_data()["lat"], map_clicked.pydeck_chart.get_selection_data()["lon"]
+# Criando um evento de clique no mapa que chama a função JavaScript
+m.add_child(folium.LatLngPopup())
 
-        if st.button("Adicionar Marcador"):
-            add_marker(lat, lon)
-if __name__ == "__main__":
-    main()
+# Criando uma função Python para criar um marcador com o Folium a partir das coordenadas do clique
+def add_marker(e):
+  folium.Marker(location=[e.latlng.lat, e.latlng.lng], popup='Marcador').add_to(m)
+
+# Adicionando um evento de clique no mapa que chama a função Python
+m.add_child(folium.ClickForMarker(popup='Marcador'))
+
+# Exibindo o mapa com o streamlit-folium
+folium_static(m)
